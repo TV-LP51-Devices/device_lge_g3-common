323b2292a65f8c3b2c87995a899e8d2460c028ab
oadam11 <ovadia.adam@gmail.com>
2015-11-24 17:00:08 -0500

    use uber 5.2 tc for rom 	modified:   BoardConfigCommon.mk
    Change-Id: I09c533ba72f92ba49fc7e4a6fdc06f9a10a11a0e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,7 +51,7 @@ KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-ea
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 
 # Rom Toolchain
-TARGET_GCC_VERSION_EXP := 4.9
+TARGET_GCC_VERSION_EXP := 5.2-uber
 
 BLOCK_BASED_OTA := true
 
