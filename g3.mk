ff6aa5be4516cca5db16e7246cc51066be444e11
Dan Pasanen <invisiblek@cyanogenmod.org>
2015-12-29 21:33:32 -0600

    Revert "g3: Add boringssl-compat wrapper"
    This reverts commit c9caaa4f29300e57c53dfd2cebd57af6ab84df75.
    
    Change-Id: If88e293b2f9a553b7358e0b8709cf319be2f3635

--- g3.mk
+++ g3.mk
@@ -90,10 +90,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     bdAddrLoader
 
-# BoringSSL compatability wrapper
-PRODUCT_PACKAGES += \
-    libboringssl-compat
-
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
